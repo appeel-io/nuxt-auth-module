f4cdb304c0d84b6c1a001eea00e7818ca379bd49
Pieter <pieter.vlem@gmail.com>
2023-07-10 15:49:57 +0200

    adjust

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,12 @@ const { signIn, signOut, session, status, cookies } = useAuth()
 </script>
 
 <template>
-  <div>
+  <div class="m-10">
     <div class="mb-10">
-      <a href="/api/auth/signin" class="buttonPrimary">Native Link Sign in</a>
-      <button @click="signIn(`azure-ad`)">
+      <button v-if="status === 'unauthenticated'" class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" @click="signIn(`azure-ad`)">
         JS Sign In
       </button>
-      <button @click="signOut()">
+      <button v-else class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" @click="signOut()">
         Sign Out
       </button>
     </div>
